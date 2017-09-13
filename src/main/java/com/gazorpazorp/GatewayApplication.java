559868bea82003a6caa348b6cba7662a0372a9eb
Christopher Peckover <christopher.peckover@gmail.com>
2017-09-13 01:05:29 -0400

    Eureka settings for prod only

--- src/main/java/com/gazorpazorp/GatewayApplication.java
+++ src/main/java/com/gazorpazorp/GatewayApplication.java
@@ -2,16 +2,15 @@ package com.gazorpazorp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -39,6 +38,7 @@ public class GatewayApplication {
 	}
 	
 	@Bean
+	@Profile("!dev")
 	public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils utils) 
 	{
 //		EurekaInstanceConfigBean instance = new EurekaInstanceConfigBean(utils);
